67c17ba57db26b955366b15799bc376362f6ea1f
wangqingye <329103586@qq.com>
2017-06-28 23:06:48 +0800

    test

--- src/View/MissonSence.ts
+++ src/View/MissonSence.ts
@@ -69,7 +69,7 @@ class MissonSence extends eui.Component
             if(icon.level <= mileStone)
             {
                 icon.enabled = true;
-                console.log(3);
+                console.log(2);
                 icon['img_levelbg'].source = "gs_select_1_png";
             }else
             {
